48a674f7da7e3fa5801f437d4f7ec7c9bc6df3d2
anjiesekani <annnjeri5054@gmail.com>
2024-05-23 10:45:16 +0300

    WORKING ON FORMS

---
+++ src/app/Layout/Pages/notify/notify.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notify',
+  templateUrl: './notify.component.html',
+  styleUrl: './notify.component.css',
+})
+export class NotifyComponent {
+  onFormSubmitted() {
+    console.log();
+  }
+}
